0dc322758913212dd3c4d005bd13c64d37507777
StechWasTaken <41377140+StechWasTaken@users.noreply.github.com>
2021-04-17 23:24:25 +0200

    it works
    not satisfied with though

--- app.py
+++ app.py
@@ -21,22 +21,40 @@ class VideoForm(FlaskForm):
 def index():
     form = VideoForm()
     frames = {}
+    width = 0
+    height = 0
+    total_frames = 0
+    frame_time = 0
 
     if form.validate_on_submit():
         file = form.video.data.stream
         video = VideoCapture(file.name)
-        success, images = video.read()
-        gray_video = cvtColor(images, COLOR_BGR2GRAY)
+        width = int(video.get(CAP_PROP_FRAME_WIDTH)) // 12
+        height = int(video.get(CAP_PROP_FRAME_HEIGHT)) // 12
+        total_frames = int(video.get(CAP_PROP_FRAME_COUNT))
+        frame_time = 1000 / video.get(CAP_PROP_FPS)
 
-        count = 0
-        for frame in gray_video:
-            pixels = []
-            for pixel in frame:
-                pixels.append(pixel)
-            frames['frame_' + str(count)] = str(pixels)
-            count += 1
+        # possibly add compression to the algorithm to make processing faster!
+        i = 0
+        while True:
+            video.set(CAP_PROP_POS_FRAMES, i)
+            success, image = video.read()
+            if success:
+                try:
+                    gray_image = cvtColor(image, COLOR_BGR2GRAY)
+                    gray_image = resize(gray_image, (width, height))
+                    image = []
+                    for pixels in gray_image:
+                        for pixel in pixels:
+                            image.append(pixel)
+                    frames['frame_' + str(i)] = str(image)
+                    i += 1
+                except:
+                    break
+            else:
+                break
 
-    return render_template('index.html', form=form, frames=frames)
+    return render_template('index.html', form=form, frames=frames, size=(width, height), total_frames=total_frames, frame_time=frame_time)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
